4fc12658441479b3549e0ba01826c15475e82db4
jelauria <jaelauria@gmail.com>
2021-02-20 16:16:40 -0800

    updated python syntax reference

---
+++ test/syntax.py
@@ -0,0 +1,5 @@
+# gather user input and format it into a string
+animal = input("Enter your favorite animal: ")
+sound = input("What sound does that animal make? ")
+print("The {} says {}".format(animal, sound))
+
